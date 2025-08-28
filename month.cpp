769e1247f2922a506a0772fa5260ae2382d65a7a
ZZMHunter <zhao.zhang50@myhunter.cuny.edu>
2025-08-28 14:08:48 -0400

    fixed logics in month.cpp

--- month.cpp
+++ month.cpp
@@ -24,21 +24,36 @@ int main()
         // checks if month is feburary, and if it is a leap year to print either 28 or 29
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
         {
-            cout << 29 << " days" << endl;
+            cout << "29 days" << endl;
         }
         else
         {
-            cout << 28 << " days" << endl;
+            cout << "28 days" << endl;
         }
     }
     else if (month % 2 == 0)
     {
-        cout << 30 << " days" << endl;
+        if (month < 8)
+        {
+            cout << "30 days" << endl;
+        }
+        else
+        {
+            cout << "31 days" << endl;
+        }
     }
     else
     {
-        cout << 31 << " days" << endl;
+        if (month < 8)
+        {
+            cout << "31 days" << endl;
+        }
+        else
+        {
+            cout << "30 days" << endl;
+        }
     }
-    // for the rest of the months, if the month is odd print 31, if the month is even print 30
+    // for months before august, if the month is odd print 31, if the month is even print 30
+    // for months august and after, if the month is odd print 30, if the month is even print 31
     return 0;
 }
